7e6d9ba0660446929d6ec92e8384bfeccb4259fe
Uladzimir Kulakou <rombik9@ya.ru>
2021-06-24 19:43:00 +0300

    create Project

---
+++ HomeWork_13_Registration/HomeWork_13_Registration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork_13_Registration
+//
+//  Created by Uladzimir Kulakou on 6/24/21.
+//  Copyright © 2021 Uladzimir Kulakou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
